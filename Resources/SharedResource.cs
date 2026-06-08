12794e52676b70a0e0e0acdc23a2cf85f4c0f5c5
JasonGoh <User@LAPTOP-OVD13D7G>
2026-06-09 03:14:39 +0800

    patch 2

--- Resources/SharedResource.cs
+++ Resources/SharedResource.cs
@@ -5,10 +5,5 @@
 /// Strings live in Resources/SharedResource.resx (English default)
 /// and Resources/SharedResource.zh-Hant.resx (Traditional Chinese).
 /// </summary>
- 
 
-
-namespace DinoAI
-{
-    public class SharedResource { }
-}
+public class SharedResource { }
